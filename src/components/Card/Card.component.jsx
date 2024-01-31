9fc619fc15c043ac167215ccb5b1a8119d516137
KentChen <bboykunz@gmail.com>
2024-01-31 10:09:58 +0800

    add hint can drop field

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.styles.css";
 
 const Card = ({ task, empty }) => {
-  return <div className={`card ` + (empty ? "card--empty" : "")}>{task}</div>;
+  return <div className={`card ` + (empty ? "card__empty" : "")}>{task}</div>;
 };
 
 export default Card;
--- src/components/Column/Column.component.jsx
+++ src/components/Column/Column.component.jsx
@@ -6,6 +6,9 @@ import DraggableCard from "../DraggableCard/DraggableCard.component";
 import { useDrop } from "react-dnd";
 import { ItemTypes } from "../../Constants";
 
+//引入canDrop需求
+import Card from "../Card/Card.component";
+
 const Column = ({ tasks: { title, tasks }, columnIndex, handleMoveMyTask }) => {
   const cards = tasks.map((task, index) => {
     const propsToDraggbleCard = { task, columnIndex, index };
@@ -19,7 +22,9 @@ const Column = ({ tasks: { title, tasks }, columnIndex, handleMoveMyTask }) => {
   });
 
   //設定drop區塊
-  const [{}, dropRef] = useDrop({
+
+  //這裡為了可以設定提示拖曳區塊（canDrop），原本空物件要替換掉
+  const [{ isOver, canDrop }, dropRef] = useDrop({
     accept: ItemTypes.CARD,
     drop: item => {
       const from = item;
@@ -31,13 +36,23 @@ const Column = ({ tasks: { title, tasks }, columnIndex, handleMoveMyTask }) => {
     //這裡來確認了from是空物件
       handleMoveMyTask(from, to);
     },
-    canDrop: item => item.columnIndex !== columnIndex
+    canDrop: item => item.columnIndex !== columnIndex,
+    //多設定collect區塊
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
   });
 
   return (
     <div ref={dropRef} className="column">
       <p className="column__title">{title}</p>
-      <div className="column__cards">{cards}</div>
+      <div className="column__cards">
+        {/* 原始卡片區塊 */}
+        {cards}
+        {/* 提示拖曳區塊 */}
+        {isOver && canDrop ? <Card empty/> : ""}
+      </div>
       <div className="column__add-task-input">
         <textarea type="text" placeholder="Type task here ..." />
         <button className="column__add-task-btn">Add Task</button>
